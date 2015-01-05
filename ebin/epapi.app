426f21653e29fcd0fdd84ba9229396e447f46a77
cammcad <cameron.frederick@gmail.com>
2015-01-04 21:04:43 -0800

    initial commit of epapi

---
+++ ebin/epapi.app
@@ -0,0 +1,8 @@
+{application,epapi,
+             [{description,[]},
+              {vsn,"1"},
+              {registered,[]},
+              {applications,[kernel,stdlib]},
+              {mod,{epapi_app,[]}},
+              {env,[]},
+              {modules,[epapi_app,epapi_sup]}]}.
---
+++ src/epapi_app.erl
@@ -0,0 +1,16 @@
+-module(epapi_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    epapi_sup:start_link().
+
+stop(_State) ->
+    ok.
